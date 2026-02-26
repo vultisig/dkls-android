19b4842c269fc4a3358dd518ede456f92387c43c
Johnny Luo <johnnyluo1980@gmail.com>
2026-02-26 14:40:10 +1100

    Add Dilithium library integration with JNI bindings and initial setup

---
+++ dilithium/src/main/java/com/silencelaboratories/godilithium/BufferUtilJNI.java
@@ -0,0 +1,17 @@
+// THIS FILE IS GENERATED MANUALLY , PLEASE DON'T REMOVE IT
+package com.silencelaboratories.godilithium;
+public class BufferUtilJNI {
+    static {
+        System.loadLibrary("godilithiumswig");
+        System.loadLibrary("dilithium_buffer_util");
+    }
+
+    public final static native void set_bytes_on_tss_buffer(tss_buffer jarg1, byte[] jarg1_);
+
+    public final static native byte[] get_bytes_from_tss_buffer(tss_buffer jarg1);
+
+    public final static native void set_bytes_on_go_slice(go_slice jarg1, byte[] jarg1_);
+
+    public final static native byte[] get_bytes_from_go_slice(go_slice jarg1);
+}
+
